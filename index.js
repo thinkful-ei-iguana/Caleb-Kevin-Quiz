b7e6ac6772e8939b937c907661da030a1ea10d21
Kevin Moore <moore.kevin.p@gmail.com>
2019-10-25 16:59:06 -0400

    pseudocode added

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
 'use strict';
+ //REQUIREMENTS GO HERE
 
+ //Moves from the initial view to the first question
+function handleBegin() {
+
+    console.log('handleBegin ran');
+}
+
+//Displays confirmation of correct answer
+//OR
+//Displays that selected answer is incorrect, showing the correct answer
+//Updates score in header
+function handleSubmit () {
+
+    console.log('handleSubmit ran')
+}
+
+//Move to the next question
+//OR
+//Moves to the end page if no questions remain
+//Updates question in header
+function handleNextQuestion () {
+
+    console.log('handleNextQuestion ran');
+}
+
+//Returns to question 1 and resets the incriments for question and score
+function handleRetakeQuiz () {
+
+    console.log('handleRetakeQuiz ran')
+}
+
+//Runs all the functions when the page loads
+function handleQuiz () {
+    handleBegin();
+    handleSubmit();
+    handleNextQuestion();
+    handleRetakeQuiz();
+
+    console.log('handleQuiz ran')
+}
+
+$(handleQuiz);
--- source.js
+++ source.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const STORE = {
+  questions: [
+    {
+      question:  'How many cups are in a quart?',
+      choices: [
+        '1',
+        '2',
+        '3',
+        '4'
+      ],
+      answer: '4'
+    },
+//PUT IN MORE QUESTIONS AS OBJECTS IN THIS ARRAY
+  ],
+  currentQuestion: 0,
+  currentScore: 0
+};
