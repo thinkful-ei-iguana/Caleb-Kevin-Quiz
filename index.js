5e16380a76cb2d815928048323b08eb16bfdd7d9
Kevin Moore <moore.kevin.p@gmail.com>
2019-10-28 11:35:42 -0400

    updated question rendering

--- index.js
+++ index.js
@@ -4,32 +4,43 @@
 //Moves from the initial view to the first question
 function handleStart() {
   $('.startButton').on('click', event => {
-    $('.questionBox').children().html(`
-        <form class="questions">
-          <fieldset>
-           <legend class="questionText">Question text...</legend>
-            <label class="sizeMe" for="0">
-             <input class="radio" type="radio" id="0" value="1" name="answer" required="">
-             <span>1</span>
-            </label><label class="sizeMe" for="1">
-              <input class="radio" type="radio" id="1" value="2" name="answer" required="">
-              <span>2</span>
-            </label><label class="sizeMe" for="2">
-              <input class="radio" type="radio" id="2" value="3" name="answer" required="">
-              <span>3</span>
-            </label><label class="sizeMe" for="3">
-              <input class="radio" type="radio" id="3" value="4" name="answer" required="">
-              <span>4</span>
-            </label><button type="submit" class="submitButton button"> Submit</button>
-          </fieldset>
-        </form>`);
-        
     $('.startQuiz').addClass('hidden');
     $('.questionBox').children().removeClass('hidden');
     console.log('handleStart ran');
+    renderQuestion();
   });
 }
 
+function updateHeader() {
+  const header = $(``
+
+  )
+}
+
+function renderQuestion() {
+  let question = STORE.questions[STORE.currentQuestion];
+  //Run update Question and Score which is not written yet
+  const questionHtml = `
+   <form class="questions">
+    <fieldset>
+     <legend class="questionText">${question.question}</legend>
+      <label class="sizeMe" for="0">
+       <input class="radio" type="radio" id="0" value="1" name="answer" required="">
+       <span>${question.choices[0]}</span>
+      </label><label class="sizeMe" for="1">
+        <input class="radio" type="radio" id="1" value="2" name="answer" required="">
+        <span>${question.choices[1]}</span>
+      </label><label class="sizeMe" for="2">
+        <input class="radio" type="radio" id="2" value="3" name="answer" required="">
+        <span>${question.choices[2]}</span>
+      </label><label class="sizeMe" for="3">
+        <input class="radio" type="radio" id="3" value="4" name="answer" required="">
+        <span>${question.choices[3]}</span>
+      </label><button type="submit" class="submitButton button"> Submit</button>
+    </fieldset>
+  </form>`;
+  $('.questionBox').children().html(questionHtml);
+}
 //Displays confirmation of correct answer
 //OR
 //Displays that selected answer is incorrect, showing the correct answer
@@ -45,13 +56,13 @@ function handleSubmit () {
 //Updates question in header
 function handleNextQuestion () {
   $('.questionBox').on('submit', event => {
-  event.preventDefault();
-  //if this is the last question...
-  $('.questionBox').addClass('hidden');
-  $('.results').removeClass('hidden');
-  console.log('handleNextQuestion ran');
+    event.preventDefault();
+    //if this is the last question...
+    $('.questionBox').addClass('hidden');
+    $('.results').removeClass('hidden');
+    console.log('handleNextQuestion ran');
   });
-};
+}
 
 //Returns to question 1 and resets the incriments for question and score
 function handleRetakeQuiz () {
